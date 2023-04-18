4e53e1930d1b40572e10a202dd2b60a5df44aec9
AndreevSV <51971450+AndreevSV@users.noreply.github.com>
2023-04-18 12:23:37 +0300

    6-th commit. Добрый день! Поправил тесты, использующие моки.

--- src/test/java/com/pro/sky/usertest_junit/service/UserServiceTest.java
+++ src/test/java/com/pro/sky/usertest_junit/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.pro.sky.usertest_junit.service;
 
 import com.pro.sky.usertest_junit.model.User;
 import com.pro.sky.usertest_junit.repository.UserDao;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -30,13 +31,16 @@ public class UserServiceTest {
     @Test
     public void shouldReturnExistUser() {
         userDaoMock.addToUserList();
-        when(out.checkUserExist(USER_NAME_CORRECT)).thenReturn(true);
+        when(userDaoMock.getUserByName(USER_NAME_CORRECT)).thenReturn(USER_CORRECT);
+        Assertions.assertTrue(out.checkUserExist(USER_NAME_CORRECT));
     }
 
     @Test
     public void shouldReturnNullUser() {
         userDaoMock.addToUserList();
-        when(out.checkUserExist(USER_NAME_INCORRECT)).thenReturn(false);
+        when(userDaoMock.getUserByName(USER_NAME_INCORRECT)).thenReturn(null);
+        Assertions.assertFalse(out.checkUserExist(USER_NAME_INCORRECT));
+
     }
 
 
